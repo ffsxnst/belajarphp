a5554f7c394830febf1eaa6ee03b6a81f0f9e7cb
ffsxnst <ffsxnst@gmail.com>
2020-11-30 15:21:30 +0700

    projek pertama

---
+++ home.php
@@ -0,0 +1,20 @@
+<?php
+
+//ini komentar
+//weakly typed itu artinya tipe data kalo di java
+//html itu browser atau client
+//kalo php itu server
+$nama = "Kikong";
+echo "Welcome home ".$nama;
+?>
+
+<?php
+for($i=0; $i<10; $i++)
+{
+    echo "<h1>Halo<h1>";
+}
+
+?>
+
+<h1>Halo</h1>
+Halo
